fcfe477954de4382089324e7181f9225e6a59ce4
rafi-ahmed <smrafi120@gmail.com>
2022-11-17 23:27:10 +0600

    first commit

---
+++ EvenOdd.cpp
@@ -0,0 +1,28 @@
+/*Being a nonconformist, Volodya is displeased with the current state of things, particularly with the 
+order of natural numbers (natural number is positive integer number). He is determined to rearrange them.
+ But there are too many natural numbers, so Volodya decided to start with the first n. He writes down the 
+ following sequence of numbers: firstly all odd integers from 1 to n (in ascending order), then all even 
+ integers from 1 to n (also in ascending order). Help our hero to find out which number will stand at the 
+ position number k.*/
+ 
+ //10 3 output 5
+ //7 7 output 6
+#include<iostream>
+using namespace std;
+int main(){
+	long long int n,k,p,ans;
+	cin>>n>>k;
+	if(n%2){
+		p=(n/2)+1;
+	}else{
+		p=n/2;
+	}
+	if(k<=p){
+		ans=2*k-1;
+	}else{
+		k=k-p;
+		ans=2*k;
+	}
+	cout<<ans;
+	return 0;
+}
---
+++ queueAtTheSchool.cpp
@@ -0,0 +1,32 @@
+/*During the break the schoolchildren, boys and girls, formed a queue of n people in the canteen. 
+Initially the children stood in the order they entered the canteen. However, after a while the boys
+ started feeling awkward for standing in front of the girls in the queue and they started letting the
+girls move forward each second.
+Let's describe the process more precisely. Let's say that the positions in the queue 
+are sequentially numbered by integers from 1 to n, at that the person in the position number 1 is
+served first. Then, if at time x a boy stands on the i-th position and a girl stands on the (i + 1)-th
+position, then at time x + 1 the i-th position will have a girl and the (i + 1)-th position will have a boy. The time is given in seconds.
+You've got the initial position of the children, at the initial moment of time. Determine the way 
+the queue is going to look after t seconds.*/
+//5 1       
+//BGGBG
+//output GBGGB
+#include<iostream>
+using namespace std;
+int main(){
+	int n,t;
+	string s;
+	cin>>n>>t;
+	cin>>s;
+	while(t--){
+		for(int i=0;i<n;i++){
+			if(s[i]=='B' && s[i+1]=='G'){
+				s[i]='G';
+				s[i+1]='B';
+				
+			}
+		}
+	}
+	cout<<s;
+	return 0;
+}
